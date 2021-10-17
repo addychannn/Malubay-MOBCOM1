22d74fdb8662521b7f489d758d95e3ffa24ffefa
MalubayA <20170429@s.ubaguio.edu>
2021-10-17 17:10:55 +0800

    lab02

--- MalubayMLab02/app/src/main/java/com/example/malubaymlab02/MainActivity.kt
+++ MalubayMLab02/app/src/main/java/com/example/malubaymlab02/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.malubaymlab02
-
+//welcome
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        open order form button
+            val btnOrderForm = findViewById<Button>(R.id.btnOrderHere)
+            val context = btnOrderForm.context
+            val iNext = Intent(this,orderForm::class.java)
+            btnOrderForm?.setOnClickListener{
+                startActivity(iNext)}
     }
 }
