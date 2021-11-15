787dc0bfbfcc6ab17a91d696f55cbea116d838b4
MalubayA <20170429@s.ubaguio.edu>
2021-11-15 13:42:26 +0800

    MalubayFLab01

---
+++ MalubayFLab01/app/src/main/java/com/example/malubayflab01/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.malubayflab01
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val goToProfile=findViewById<Button>(R.id.nextBtn)
+        val context=goToProfile.context
+        val iProfile= Intent(context,profile::class.java)
+
+        goToProfile?.setOnClickListener{
+            context.startActivity(iProfile)
+        }
+    }
+}
